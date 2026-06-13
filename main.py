5cbd379677a7a4608f261c88cbc6b9350a406b82
Alan Lima <alanlimanvc@gmail.com>
2026-06-13 14:27:59 -0300

    feat(main): add main entry point

---
+++ main.py
@@ -0,0 +1,28 @@
+import cmds.ex1.client
+import cmds.ex1.server
+from   internal import net
+import sys
+
+def main():
+    if len(sys.argv) < 3:
+        print("usage sprout <exercise> ( client | server ) <ip>:<port>")
+        sys.exit(1)
+
+    addr = net.ParseAddr(sys.argv[3])
+    if not addr.Ok():
+        print(f"invalid addr {addr.Err()}")
+        return
+
+    ex, dom = sys.argv[1], sys.argv[2]
+    addr = addr.Val()
+
+    match ex, dom:
+        case ("ex1", "client"): cmds.ex1.client.Handle(addr)
+        case ("ex1", "server"): cmds.ex1.server.Handle(addr)
+
+        case _:
+            print("unknown exercise")
+            sys.exit(1)
+
+if __name__ == "__main__":
+    main()
