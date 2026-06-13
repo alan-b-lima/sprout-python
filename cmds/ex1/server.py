2f75d845177aa963eaaeb250a403912e33bc2323
Alan Lima <alanlimanvc@gmail.com>
2026-06-13 14:23:53 -0300

    feat(ex1/server): refactor connection handling with Handler class

--- cmds/ex1/server.py
+++ cmds/ex1/server.py
@@ -1,5 +1,6 @@
-from internal import net, tcp
-from pkg      import log, routine, utf8
+from internal  import net, tcp
+from pkg       import log, routine, utf8
+from pkg.error import Result, Ok, Err, error
 
 def Handle(addr: net.Addr) -> None:
     ln = tcp.Listen(addr)
@@ -19,7 +20,7 @@ def Handle(addr: net.Addr) -> None:
                 log.Error(conn.Err())
                 continue
 
-            routine.Go(lambda: _handle(conn.Val()))
+            routine.Go(Handler(conn.Val()))
 
     except KeyboardInterrupt:
         print("\r", end="")
@@ -33,48 +34,88 @@ ETX = b"\003"
 MSG_OK  = b"OK\n"
 MSG_BYE = b"BYE\n"
 
-def _handle(conn: tcp.Conn) -> None:
-    ip, port = conn.Addr()
-    log.Info(f"established connection at {ip}:{port}")
+class Handler:
+    _conn: tcp.Conn
 
-    with conn:
-        acc = bytes()
+    _acc: bytearray
+    _buf: bytearray
+    _etx: bool
 
-        should_close = False
-        while not (conn.Done() or should_close):
-            b = conn.Read(4096)
-            if not b.Ok():
-                log.Error(b.Err())
+    def __init__(self, conn: tcp.Conn) -> None:
+        self._conn = conn
+        self._acc  = bytearray()
+        self._buf  = bytearray(4096)
+        self._etx  = False
+
+    def __call__(self) -> None:
+        with self._conn:
+            ip, port = self._conn.RemoteAddr()
+            addr = f"{ip}:{port}"
+
+            log.Info(addr, "connection established")
+            self._handle(addr)
+            log.Info(addr, "connection terminated")
+
+    def _handle(self, addr: str) -> None:
+        while not self._conn.Done() and not self._etx:
+            line = self._read_line()
+            if not line.Ok():
+                log.Error(line.Err())
                 break
 
-            err = conn.Write(MSG_OK)
-            if not err.Ok():
-                log.Error(err.Err())
-                break
-
-            b = b.Val()
-            index = b.find(ETX)
-            if index >= 0:
-                acc += b[index:]
-                should_close = True
+            if self._etx:
+                log.Info(addr, "received end of text")
+                err = self._conn.Write(MSG_BYE)
             else:
-                acc += b
+                err = self._conn.Write(MSG_OK)
 
-            while True:
-                index = acc.find(LF)
-                if index == -1:
-                    break
+            if not err.Ok():
+                log.Error(addr, err.Err())
+                break
 
-                str = utf8.Decode(acc[:index])
-                if not str.Ok():
-                    log.Error(f"malformed message from {ip}:{port}")
-                    continue
+            line = line.Val()
+            if len(line) == 0:
+                continue
 
-                log.Info(f"message from {ip}:{port}: {str.Val()}")
-                acc = acc[index+1:]
+            log.Info(addr, f"message: {line}")
 
-        err = conn.Write(MSG_BYE)
-        if not err.Ok():
-            log.Warn(err.Err())
+    def _read_line(self) -> Result[str, Exception]:
+        index = self._find(self._acc)
+        if index != -1:
+            line, self._acc = self._acc[:index], self._acc[index+1:]
+            return self._encode(line)
 
-    log.Info(f"closed connection to {ip}:{port}")
+        while True:
+            n = self._conn.Read(self._buf)
+            if not n.Ok():
+                return Err(n.Err())
+            
+            n = n.Val()
+            buf = self._buf[:n]
+
+            index = self._find(buf)
+            if index != 1:
+                line = self._acc + buf[:index]
+                self._acc = bytearray(buf[index+1:])
+
+                return self._encode(line)
+    
+            self._acc += buf
+
+    def _find(self, buf: bytes) -> int:
+        for i, byte in enumerate(buf):
+            if byte in LF:
+                return i
+
+            if byte in ETX:
+                self._etx = True
+                return i
+
+        return -1
+    
+    def _encode(self, line: bytes) -> Result[str, Exception]:
+        str = utf8.Decode(line)
+        if not str.Ok():
+            return Err(error(f"malformed message"))
+        
+        return Ok(str.Val())
