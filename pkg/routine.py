e1bf0a40aac870ffd414b5809340725ec241c4e8
Alan Lima <alanlimanvc@gmail.com>
2026-06-13 22:29:09 -0300

    feat(routine): implement Pool class for worker threads

--- pkg/routine.py
+++ pkg/routine.py
@@ -1,10 +1,44 @@
-from   pkg.error import error
+import queue
 import threading
 from   typing    import Callable
 
 def Go(func: Callable[[], None]) -> None:
     if not callable(func):
-        raise error("not callable")
+        raise ValueError("not callable")
 
     thread = threading.Thread(None, func, daemon=True)
     thread.start()
+
+class Pool:
+    _cond: threading.Condition
+    _work: queue.Queue[Callable[[], None]]
+
+    def __init__(self, workers: int) -> None:
+        self._cond = threading.Condition()
+        self._work = queue.Queue(workers)
+        self._open = True
+
+        for _ in range(workers):
+            Go(self._routine)
+
+    def Go(self, func: Callable[[], None]) -> None:
+        with self._cond:
+            while self._work.full():
+                self._cond.wait()
+            
+            self._work.put(func)
+            self._cond.notify()
+
+    def Close(self) -> None:
+        self._open = False
+
+    def _routine(self) -> None:
+        while self._open:
+            with self._cond:
+                while self._work.empty():
+                    self._cond.wait()
+                
+                func = self._work.get()
+    
+                func()
+                self._cond.notify()
