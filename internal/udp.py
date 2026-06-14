d90ecb4c0273bcab2f7decb0582e0c081b69e418
Alan Lima <alanlimanvc@gmail.com>
2026-06-13 22:53:11 -0300

    feat(udp): implement Conn

--- internal/udp.py
+++ internal/udp.py
@@ -2,6 +2,45 @@ from   internal  import net
 from   pkg.error import Result, Ok, Err, error
 import socket
 
+class Conn:
+    _laddr: net.Addr
+    _raddr: net.Addr
+    _sock:  socket.socket
+
+    def __init__(self, sock: socket.socket) -> None:
+        self._laddr = sock.getsockname()
+        self._raddr = sock.getpeername()
+        self._sock  = sock
+
+    def Read(self, data: bytearray) -> Result[int, Exception]:
+        try:
+            return Ok(self._sock.recv_into(data, len(data)))
+
+        except OSError as ex:
+            return Err(error("read", ex))
+        
+    def Write(self, b: bytes) -> Result[int, Exception]:
+        try:
+            return Ok(self._sock.send(b))
+
+        except OSError as ex:
+            return Err(error("write", ex))
+    
+    def LocalAddr(self) -> net.Addr:
+        return self._laddr
+        
+    def RemoteAddr(self) -> net.Addr:
+        return self._raddr
+
+    def Close(self) -> None:
+        net.Shutdown(self._sock)
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, ex_t: object, ex_v: object, tb: object):
+        self.Close()
+
 class Listener:
     _addr: net.Addr
     _sock: socket.socket
@@ -12,7 +51,7 @@ class Listener:
 
     def ReadFrom(self, data: bytearray) -> Result[tuple[int, net.Addr], Exception]:
         try:
-            n, addr = self._sock.recvfrom_into(data, len(data))
+            n, addr = self._sock.recvfrom_into(data)
             return Ok((n, addr))
 
         except OSError as ex:
@@ -36,6 +75,17 @@ class Listener:
 
     def __exit__(self, ex_t: object, ex_v: object, tb: object):
         self.Close()
+
+def Dial(addr: net.Addr) -> Result[Conn, Exception]:
+    try:
+        fd = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        fd.connect(addr)
+
+    except OSError as ex:
+        ip, port = addr
+        return Err(error(f"dial at {ip}:{port}", ex))
+    
+    return Ok(Conn(fd))
 
 def Listen(addr: net.Addr) -> Result[Listener, Exception]:
     try:
