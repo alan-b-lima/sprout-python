b27bffdcc42bb892739b8950ae5d6de6ddf4be61
Alan Lima <alanlimanvc@gmail.com>
2026-06-13 22:55:22 -0300

    feat(ex2/client): implement client

---
+++ cmds/ex2/client.py
@@ -0,0 +1,73 @@
+from internal  import net, udp
+from pkg       import utf8
+from pkg.error import Result, Ok, Err
+
+KiB = 1024
+
+def Handle(addr: net.Addr) -> None:
+    conn = udp.Dial(addr)
+    if not conn.Ok():
+        print(f"error: {conn.Err()}")
+        return
+
+    conn = conn.Val()
+
+    lip, lport = conn.LocalAddr()
+    rip, rport = conn.RemoteAddr()
+
+    print(f"client started at {lip}:{lport} to {rip}:{rport}")
+
+    buf = bytearray()
+
+    try:
+        while True:
+            str = input("> ").strip()
+            if str == "/exit":
+                break
+
+            if len(str) == 0:
+                continue
+
+            b = _tobytes(str)
+            if not b.Ok():
+                print(f"error: {b.Err()}")
+                continue
+
+            b = b.Val()
+            err = conn.Write(b)
+            if not err.Ok():
+                print(f"error: {err.Err()}")
+                continue
+
+            if len(b) > len(buf):
+                buf = bytearray(len(b))
+
+            n = conn.Read(buf)
+            if not n.Ok():
+                print(f"error: {n.Err()}")
+                continue
+
+            str = utf8.Decode(buf[:n.Val()])
+            if not str.Ok():
+                print("malformed response")
+
+            print(str.Val())
+
+    except KeyboardInterrupt:
+        print("\r", end="")
+
+    conn.Close()
+
+def _tobytes(s: str) -> Result[bytes, Exception]:
+    if len(s) > 64*KiB:
+        return Err(ValueError("message too long"))
+
+    b = utf8.Encode(s)
+    if not b.Ok():
+        return Err(ValueError("malformed string"))
+    
+    b = b.Val()
+    if len(b) > 64*KiB:
+        return Err(ValueError("message too long"))
+
+    return Ok(b)
