a92638203803cf50935c5ee4bd4066c26a0131b4
Alan Lima <alanlimanvc@gmail.com>
2026-06-15 12:34:28 -0300

    feat(main): update command line arguments to support exercise 10 client and server

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import cmds.ex2.client
 import cmds.ex2.server
 import cmds.ex4.client
 import cmds.ex4.server
+import cmds.ex10.client
+import cmds.ex10.server
 from   internal import net
 import sys
 
 def main():
-    if len(sys.argv) < 3:
-        print("usage sprout <exercise> ( client | server ) <ip>:<port>")
+    if len(sys.argv) < 4:
+        print("usage sprout <exercise> ( client | server ) <ip>:<port> [ <name> ]")
         sys.exit(1)
 
     addr = net.ParseAddr(sys.argv[3])
@@ -20,6 +22,10 @@ def main():
     ex, dom = sys.argv[1], sys.argv[2]
     addr = addr.Val()
 
+    name = ""
+    if len(sys.argv) >= 5:
+        name = sys.argv[4]
+
     match ex, dom:
         case ("ex1", "client"): cmds.ex1.client.Handle(addr)
         case ("ex1", "server"): cmds.ex1.server.Handle(addr)
@@ -29,6 +35,9 @@ def main():
 
         case ("ex4", "client"): cmds.ex4.client.Handle(addr)
         case ("ex4", "server"): cmds.ex4.server.Handle(addr)
+
+        case ("ex10", "client"): cmds.ex10.client.Handle(addr, name)
+        case ("ex10", "server"): cmds.ex10.server.Handle(addr)
 
         case _:
             print("unknown exercise")
