f814153370548abc6adcd1ed096810ded974f52d
Alan Lima <alanlimanvc@gmail.com>
2026-06-15 17:40:28 -0300

    refactor(main): move import statements inside match cases for better encapsulation

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-import cmds.ex1.client
-import cmds.ex1.server
-import cmds.ex2.client
-import cmds.ex2.server
-import cmds.ex4.client
-import cmds.ex4.server
-import cmds.ex10.client
-import cmds.ex10.server
 from   internal import net
 import sys
 
@@ -27,17 +19,40 @@ def main():
         name = sys.argv[4]
 
     match ex, dom:
-        case ("ex1", "client"): cmds.ex1.client.Handle(addr)
-        case ("ex1", "server"): cmds.ex1.server.Handle(addr)
+        case ("ex1", "client"):
+            import cmds.ex1.client
+            cmds.ex1.client.Handle(addr)
+        case ("ex1", "server"):
+            import cmds.ex1.server
+            cmds.ex1.server.Handle(addr)
 
-        case ("ex2", "client"): cmds.ex2.client.Handle(addr)
-        case ("ex2", "server"): cmds.ex2.server.Handle(addr)
+        case ("ex2", "client"): 
+            import cmds.ex2.client
+            cmds.ex2.client.Handle(addr)
+        case ("ex2", "server"): 
+            import cmds.ex2.server
+            cmds.ex2.server.Handle(addr)
 
-        case ("ex4", "client"): cmds.ex4.client.Handle(addr)
-        case ("ex4", "server"): cmds.ex4.server.Handle(addr)
+        case ("ex3", "client"): 
+            import cmds.ex3.client
+            cmds.ex3.client.Handle(addr, name)
+        case ("ex3", "server"): 
+            import cmds.ex3.server
+            cmds.ex3.server.Handle(addr)
 
-        case ("ex10", "client"): cmds.ex10.client.Handle(addr, name)
-        case ("ex10", "server"): cmds.ex10.server.Handle(addr)
+        case ("ex4", "client"): 
+            import cmds.ex4.client
+            cmds.ex4.client.Handle(addr)
+        case ("ex4", "server"): 
+            import cmds.ex4.server
+            cmds.ex4.server.Handle(addr)
+
+        case ("ex10", "client"): 
+            import cmds.ex10.client
+            cmds.ex10.client.Handle(addr, name)
+        case ("ex10", "server"): 
+            import cmds.ex10.server
+            cmds.ex10.server.Handle(addr)
 
         case _:
             print("unknown exercise")
